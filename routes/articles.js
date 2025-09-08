8f666a214eb6df252c0486fc4a8cf2172cfbebac
appteam020 <appteam020@gmail.com>
2025-09-08 16:00:44 +0300

    fix

--- routes/articles.js
+++ routes/articles.js
@@ -51,7 +51,19 @@ router.get('/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
-
+// @route   GET /api/articles/myarticles
+// @desc    جلب المقالات التي كتبها المستخدم الحالي (اقتباساتي)
+// @access  Private
+router.get('/myarticles', authMiddleware, async (req, res) => {
+    try {
+        // البحث عن المقالات بناءً على ID المستخدم المأخوذ من التوكن
+        const articles = await Article.find({ author: req.user.id }).sort({ createdAt: -1 });
+        res.json(articles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 // @route   GET api/articles/:id
 // @desc    الحصول على مقال واحد بواسطة ID
 // @access  Public
@@ -74,19 +86,7 @@ router.get('/:id', async (req, res) => {
       res.status(500).send('Server Error');
     }
   });
-// @route   GET /api/articles/myarticles
-// @desc    جلب المقالات التي كتبها المستخدم الحالي (اقتباساتي)
-// @access  Private
-router.get('/myarticles', authMiddleware, async (req, res) => {
-    try {
-        // البحث عن المقالات بناءً على ID المستخدم المأخوذ من التوكن
-        const articles = await Article.find({ author: req.user.id }).sort({ createdAt: -1 });
-        res.json(articles);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+
 // ==========================================================
 
 // @route   PUT api/articles/:id
